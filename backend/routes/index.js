23329f6f9167b7102606ad673aa7b03399ae48ba
Jimson Ma <ma.jimson@gmail.com>
2021-05-19 15:35:19 -0700

    added api routes testing

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,13 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
 
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
 });
+
+router.use('/api', apiRouter);
 
 module.exports = router;
