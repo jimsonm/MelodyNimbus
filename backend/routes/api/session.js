71eca36c50502d8d2a294f79ab830072058bfcb5
Jimson Ma <ma.jimson@gmail.com>
2021-05-20 11:17:47 -0700

    finished backend

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const asyncHandler = require('express-async-handler');
-
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const { check } = require('express-validator');
