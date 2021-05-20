25f25cee131ab44e812f861ccb3a7967e98ef7fd
Jimson Ma <ma.jimson@gmail.com>
2021-05-20 10:54:21 -0700

    finished react frontend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,10 +3,14 @@ const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+// router.get('/hello/world', function (req, res) {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     res.send('Hello World!');
+// });
+
+// router.post('/test', function (req, res) {
+//     res.json({ requestBody: req.body });
+// });
 
 router.use('/api', apiRouter);
 
