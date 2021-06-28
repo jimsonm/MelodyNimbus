fdcf3b40a77b6c60240f713495608b3769adcb28
Jimson Ma <ma.jimson@gmail.com>
2021-06-28 10:03:07 -0700

    fixed small bug

--- frontend/src/components/Upload/Upload.js
+++ frontend/src/components/Upload/Upload.js
@@ -202,7 +202,7 @@ function Upload() {
                                     </div>
                                     <input
                                         type='text'
-                                        value={`https://melodynimbus.herokuapp.com/users/${sessionUser.id}/${lastUpload.track_name}`}
+                                        value={`https://melodynimbus.herokuapp.com/users/${sessionUser.id}/${lastUpload.id}`}
                                         className='shareTrackTextbox'
                                         onClick={highlight}
                                     />
